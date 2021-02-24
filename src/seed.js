8c1758db5ac957dfb49732b89a0a72aaac3fa50b
Amanda Monteiro <a3monteiro@gmail.com>
2021-02-23 23:16:40 -0500

    header component

--- src/seed.js
+++ src/seed.js
@@ -1,39 +1,39 @@
 export function seedDatabase(firebase) {
     const users = [
         {
-            userId: 'Dps0lI6BSeXjafPRSVai3duKCpC2',
-            userName: 'amandamcodes',
-            fullName: 'Amanda Monteiro',
-            emailAddress: 'a3monteiro@gmail.com',
+            userId: 'bmjpkU8bBWRse2JwKgzOGTv5tUI3',
+            username: 'karl',
+            fullName: 'Karl Hadwen',
+            emailAddress: 'karlhadwen@gmail.com',
             following: ['2'],
             followers: ['2', '3', '4'],
             dateCreated: Date.now()
         },
         {
             userId: '2',
-            userName: 'ernie.a',
-            fullName: 'Ernie Anders',
-            emailAddress: 'earnie@anders.com',
+            username: 'raphael',
+            fullName: 'Raffaello Sanzio da Urbino',
+            emailAddress: 'raphael@sanzio.com',
             following: [],
-            followers: ['Dps0lI6BSeXjafPRSVai3duKCpC2'],
+            followers: ['bmjpkU8bBWRse2JwKgzOGTv5tUI3'],
             dateCreated: Date.now()
         },
         {
             userId: '3',
-            userName: 'pricedani',
-            fullName: 'Dani Price',
-            emailAddress: 'dani@price.com',
-            following: [''],
-            followers: ['Dps0lI6BSeXjafPRSVai3duKCpC2'],
+            username: 'dali',
+            fullName: 'Salvador Dalí',
+            emailAddress: 'salvador@dali.com',
+            following: [],
+            followers: ['bmjpkU8bBWRse2JwKgzOGTv5tUI3'],
             dateCreated: Date.now()
         },
         {
             userId: '4',
-            userName: 'lisas',
-            fullName: 'Lisa Sampson',
-            emailAddress: '',
-            following: [''],
-            followers: ['Dps0lI6BSeXjafPRSVai3duKCpC2'],
+            username: 'orwell',
+            fullName: 'George Orwell',
+            emailAddress: 'george@orwell.com',
+            following: [],
+            followers: ['bmjpkU8bBWRse2JwKgzOGTv5tUI3'],
             dateCreated: Date.now()
         }
     ];
@@ -44,27 +44,27 @@ export function seedDatabase(firebase) {
 
     for (let i = 1; i <= 5; ++i) {
         firebase
-        .firestore()
-        .collection('photos')
-        .add({
-            photoId: i,
-            userId: '2',
-            imageSrc: `/images/users/raphael/${i}.jpg`,
-            caption: 'Saint George and the Dragon',
-            likes: [],
-            comments: [
-                {
-                    displayName: 'dali',
-                    comment: 'Love this place, looks like my animal farm!'
-                },
-                {
-                    displayName: 'orwell',
-                    comment: 'Would you mind if I used this picture?'
-                }
-            ],
-            userLatitude: '40.7128',
-            userLongitude: '74.0060',
-            dateCreated: Date.now()
-        });
+            .firestore()
+            .collection('photos')
+            .add({
+                photoId: i,
+                userId: '2',
+                imageSrc: `/images/users/raphael/${i}.jpg`,
+                caption: 'Saint George and the Dragon',
+                likes: [],
+                comments: [
+                    {
+                        displayName: 'dali',
+                        comment: 'Love this place, looks like my animal farm!'
+                    },
+                    {
+                        displayName: 'orwell',
+                        comment: 'Would you mind if I used this picture?'
+                    }
+                ],
+                userLatitude: '40.7128°',
+                userLongitude: '74.0060°',
+                dateCreated: Date.now()
+            });
     }
 }
